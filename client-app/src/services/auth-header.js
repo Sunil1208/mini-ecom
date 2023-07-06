f25bd0914aaccce7a946cda4da55d5b12641561a
Sunil Kumar <skmy03@gmail.com>
2023-07-06 18:48:52 +0530

    fixed headers issue

--- client-app/src/services/auth-header.js
+++ client-app/src/services/auth-header.js
@@ -2,7 +2,7 @@ export default function authHeader() {
     let user = JSON.parse(localStorage.getItem("mini_ecom_user"));
   
     if (user && user.accessToken) {
-      return { "x-access-token": "Bearer " + user.accessToken };
+      return { "x-access-token": user.accessToken };
     } else {
       return {};
     }
